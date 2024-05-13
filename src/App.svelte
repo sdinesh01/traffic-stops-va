6a71c211e4ec9d73d01eeff51956452e2b40f478
Shradha Dinesh <108999498+sdinesh01@users.noreply.github.com>
2024-05-13 11:50:59 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -25,7 +25,7 @@
 	import LineChart from "../src/chartComponents/line-chart/LineChart.svelte";
 	import LineChartMini from "../src/chartComponents/line-chart-mini/LineChart.svelte";
 	import Circles from "../src/chartComponents/circle-pack/Circles.svelte";
-	import Calendar from "../src/chartComponents/calendar-chart/Calendar.svelte";
+	//import Calendar from "../src/chartComponents/calendar-chart/Calendar.svelte"; FUTURE ADDITION
 
 	// Set theme globally (options are 'light', 'dark' or 'lightblue') -- I'm not 100% done with the dark config
 	let theme = "light";
@@ -65,13 +65,24 @@
 </Header>
 
 <Filler theme="lightgreen" short={true} wide={true} center={false}>
-	<p class="text-big">
-		This is a large, left-aligned text caption
+	<p class="small">
+		In 2020, Virginia passed the Virginia Community Policing Act, which mandated the creation of a database cataloging all instances of traffic stops, stop and frisks, and detentions during traffic stops. Jurisdictions are required to contribute data to the state database. The bill established a mandated reporting system to standardize data across the state to measure “bias-based policing” in traffic stops. Per statute § 52-30.2, police are required to report: 
+	</p>
+	<p class='text-small'>
+	(i) the race, ethnicity, age, gender of the person stopped, and whether the person stopped spoke English; (ii) the reason for the stop; (iii) the location of the stop; (iv) whether a warning, written citation, or summons was issued or whether any person was arrested; (v) if a warning, written citation, or summons was issued or an arrest was made, the warning provided, violation charged, or crime charged; (vi) whether the vehicle or any person was searched; and (vii) whether the law-enforcement officer or State Police officer used physical force against any person and whether any person used physical force against any officers.
+	</p>
+	<p>
+	Also in the aftermath of the murder of George Floyd in 2020, Virginia passed a law prohibiting police from making traffic stops for minor infractions such as tinted windows, dangling objects in the rearview mirror, and smelling marijuana. While many of these infractions remain illegal, they cannot be the sole motivation for initiating a traffic stop. Advocates hoped this new legislation would decrease “pretextual policing,” which disproportionately affects racial and ethnic minorities.
 	</p>
 </Filler>
 
 <div class="center">
 	<h2>Where are the traffic stops over time?</h2>
+	<div class="left-text">
+	<p> 
+		The data was collected between July 2020 and September 2023. Virginia State Police are visualized separately. These numbers represent raw counts and have not yet been normalized by jurisdiction. Jurisdiction data per locality is challenging to collect because of a lack of standardization between municipality/local government boundaries and police forces.
+	</p>
+	</div>
 </div>
 <div class="row">
 	<div class="column first-column">
@@ -84,7 +95,11 @@
 
 <div class="center">
 	<h2>What are the outcomes of traffic outcomes by race?</h2>
-	<br>
+	<div class="left-text">
+	<p> 
+		The data for this chart was aggregated between July 2021- Sept. 2023, after police were mandated by law to report demographic information. A future iteration of this graphic will allow functionality to toggle between gender, race, age, and English speaking ability. 
+	</p>
+</div>
 </div>
 <div class="col-wide"><hr class="svelte-1l2to1w" style="color: rgb(119, 119, 119);"> 
 <Section>
@@ -105,6 +120,12 @@
 
 
 <style>
+
+	.left-text {
+		text-align: justify;
+  		text-justify: inter-word;
+		margin: 0px 40px 0.5px 40px; 
+	}
 	.row {
 			display: flex;
 		}
@@ -129,3 +150,4 @@
 		pointer-events: all !important;
 	}
 </style>
+
